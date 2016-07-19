4a76c8ac282a5f84be8735b29d9d6f43de1251ae
pogorilchuk <pogorilchuk86@gmail.com>
2016-07-19 10:18:53 +0300

    My second commit

---
+++ View/Layout.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <title>Document</title>
+</head>
+<body>
+	<a href="/">Home</a>
+	<a href="#">Books</a>
+	<a href="#">Contact</a>
+	<a href="#"><? echo CONTENT ?></a>
+<hr>
+
+</body>
